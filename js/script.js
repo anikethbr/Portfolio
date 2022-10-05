64fcca21fc21886d393a8118b9487408dd820c10
anikethbr <anikethbr73@gmail.com>
2022-10-05 18:13:30 +0530

    added scripts

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 220){
+            $('nav').addClass("sticky");
+            $('span').addClass('changeColor');
+        }else{
+            $('nav').removeClass("sticky");
+            $('span').removeClass('changeColor');
+        }
+    })
+});
